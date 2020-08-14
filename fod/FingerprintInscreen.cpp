4b646887fb057924a1d15904fcdef0a8f0375398
ALKALiKong <alikongalk@gmail.com>
2020-08-14 15:44:18 +0800

    sdm845-common: Bringup Project-404

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -50,7 +50,7 @@ static void set(const std::string& path, const T& value) {
 }  // anonymous namespace
 
 namespace vendor {
-namespace lineage {
+namespace p404 {
 namespace biometrics {
 namespace fingerprint {
 namespace inscreen {
@@ -142,5 +142,5 @@ Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallb
 }  // namespace inscreen
 }  // namespace fingerprint
 }  // namespace biometrics
-}  // namespace lineage
+}  // namespace p404
 }  // namespace vendor
